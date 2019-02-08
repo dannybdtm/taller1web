dd9f7b7d4529ba1bda1e717fdac7765347459019
DB U <>
2019-02-07 19:29:39 -0500

    INCLUIR UNA LIBRERIA JODA TIME

---
+++ mavenproject1/src/main/java/main.java
@@ -0,0 +1,23 @@
+
+
+import org.joda.time.LocalTime;
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author BOG-A404-E-008
+ */
+public class main {
+
+    public static void main(String[] args) {
+        LocalTime tiempo = new LocalTime();
+        System.out.println("El tiempo actual es:  " + tiempo);
+
+    }
+
+}
